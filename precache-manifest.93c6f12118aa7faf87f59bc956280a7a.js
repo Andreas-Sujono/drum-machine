3bc4ba8cc33fba06242520fd9471854bdf51a7c6
Andreas_Sujono <andreassujono@gmail.com>
2020-02-17 02:01:08 +0800

    Updates

---
+++ precache-manifest.93c6f12118aa7faf87f59bc956280a7a.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "22d488876e67725d5684b7949788bd2b",
+    "url": "/drum-machine/index.html"
+  },
+  {
+    "revision": "19aace3a1c9947e88f7a",
+    "url": "/drum-machine/static/css/main.c2b0a511.chunk.css"
+  },
+  {
+    "revision": "def127bc6ebc8db948a8",
+    "url": "/drum-machine/static/js/2.b98de1b4.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/drum-machine/static/js/2.b98de1b4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "19aace3a1c9947e88f7a",
+    "url": "/drum-machine/static/js/main.ea461cb3.chunk.js"
+  },
+  {
+    "revision": "507a9231a935b1c59061",
+    "url": "/drum-machine/static/js/runtime-main.e050a463.js"
+  },
+  {
+    "revision": "6b953b75ddd538b2cfbc3d89f277a5d0",
+    "url": "/drum-machine/static/media/Imagine Dragons - Whatever It Takes.6b953b75.mp3"
+  },
+  {
+    "revision": "3f05d22d478144e1e808ec1cb0f9cac4",
+    "url": "/drum-machine/static/media/Sabrina Carpenter - Why.3f05d22d.mp3"
+  },
+  {
+    "revision": "b5f213364d8bc942495063d5bdaf2639",
+    "url": "/drum-machine/static/media/Zedd-Daisy.b5f21336.mp3"
+  },
+  {
+    "revision": "244409ac28a8e3fa7dd6c58116b181ba",
+    "url": "/drum-machine/static/media/Zedd-Illusion.244409ac.mp3"
+  },
+  {
+    "revision": "ade60bd29111cc2693e8eee051b95c72",
+    "url": "/drum-machine/static/media/Zedd-Papercut.ade60bd2.mp3"
+  },
+  {
+    "revision": "610f0942f97abc6c995d8bc41c2df8a5",
+    "url": "/drum-machine/static/media/clap.610f0942.mp3"
+  },
+  {
+    "revision": "df52b6bf1c28de086c0f06841a008360",
+    "url": "/drum-machine/static/media/closeHiHat.df52b6bf.wav"
+  },
+  {
+    "revision": "071d9eb7a84e688aec4496efeabbaedb",
+    "url": "/drum-machine/static/media/cymbal.071d9eb7.wav"
+  },
+  {
+    "revision": "5524e1368dfb97428e36b105a6860f14",
+    "url": "/drum-machine/static/media/kickBass.5524e136.mp3"
+  },
+  {
+    "revision": "cdfc6c02aece6bbd06353409ce59af9f",
+    "url": "/drum-machine/static/media/openHiHat.cdfc6c02.wav"
+  },
+  {
+    "revision": "c2b209c2a9db74788163ed8d5bccb531",
+    "url": "/drum-machine/static/media/snare1.c2b209c2.wav"
+  },
+  {
+    "revision": "862a0e41d349357aabbfd1cdc4bdf13a",
+    "url": "/drum-machine/static/media/snare2.862a0e41.wav"
+  },
+  {
+    "revision": "39370c355dd6635a45fc92d049070bce",
+    "url": "/drum-machine/static/media/snare3.39370c35.wav"
+  },
+  {
+    "revision": "36b20edd2dcb42cf36451522e31b236e",
+    "url": "/drum-machine/static/media/snare4.36b20edd.wav"
+  }
+]);
